fe97f6d7b0d4151c360d0088d65cb80de9354c1c
gzcaue <cauegeraldizanardi@gmail.com>
2026-08-03 16:07:48 -0300

    feat: cria o método estudantesAprovados

--- estudante/src/Main.java
+++ estudante/src/Main.java
@@ -1,9 +1,16 @@
 package src;
 
+import entities.Estudante;
+
 import java.util.Locale;
 
 public class Main {
     static void main(String[] args) {
+
+    }
+
+    public Estudante[] estudantesAprovados(Estudante[] estudantes){
+        Estudante[] retorno = new Estudante[estudantes.length];
 
     }
 }
